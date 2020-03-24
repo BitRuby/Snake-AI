78bc00d17cb5cf8651f27910fdcfcf6412fa0c1a
BitRuby <firewallosiem@gmail.com>
2020-03-24 11:24:27 +0100

    Added initialize functions

--- server/src/network.ts
+++ server/src/network.ts
@@ -1,6 +1,10 @@
 import Population from "./genetic_algorithm/population";
 import Individual from "./genetic_algorithm/individual";
 
+interface MapSettings {
+    width: number;
+    height: number;
+}
 interface Position {
     x: number;
     y: number;
@@ -17,6 +21,8 @@ interface MovementRegister {
 interface CurrentMovement {
     snakePos: Array<Position>;
     applePos: Position;
+    headDirection: string;
+    tailDirection: string;
 }
 
 export default class Network {
@@ -24,12 +30,13 @@ export default class Network {
     private population: Population;
     private movementRegister: MovementRegister;
     private currentMovement: CurrentMovement;
-
-    constructor() {
+    private mapSettings: MapSettings;
+    constructor(mapSettings: MapSettings) {
         this.population = new Population(4);
         this.dead = false;
         this.movementRegister = {} as MovementRegister;
         this.currentMovement = {} as CurrentMovement;
+        this.mapSettings = mapSettings;
     }
 
     encodeNetworkInputs = () => {
@@ -40,9 +47,57 @@ export default class Network {
 
     }
 
+    updateSnakePosition = () => {
+        
+    }
+
+    randomApple = (initial: boolean = false): Position => {
+        let randomApple: Position;
+        for (; ;) {
+            randomApple = {
+                x:
+                    (Math.floor(Math.random() * this.mapSettings.width + 0.99)),
+                y:
+                    (Math.floor(Math.random() * this.mapSettings.height + 0.99))
+            };
+            if (
+                !this.currentMovement.snakePos.some(e => e.x === randomApple.x && e.y === randomApple.y)
+            ) {
+                break;
+            }
+            if (initial && (randomApple.x === 0 && randomApple.y === 0)) {
+                continue;
+            }
+        }
+        return randomApple;
+    }
+
+    initializeSnakePosition = () => {
+        this.movementRegister = {} as MovementRegister;
+        this.currentMovement = {} as CurrentMovement;
+        const applePos = this.randomApple(true);
+        this.movementRegister = {
+            id: 0,
+            motion: [{
+                snakePos: { x: 0, y: 0 },
+                applePos: applePos,
+                points: 0
+            }]
+        }
+        this.currentMovement = {
+            snakePos: [{ x: 0, y: 0 }],
+            applePos: applePos,
+            headDirection: 'right',
+            tailDirection: 'left'
+        }
+    }
+
     makeAMove = (weights: Array<number>): void => {
+        this.initializeSnakePosition();
         while (!this.dead) {
-            this.movementRegister.motion.push();
+            this.encodeNetworkInputs();
+            this.calculateNetwork(weights);
+            this.updateSnakePosition();
         }
     }
 
